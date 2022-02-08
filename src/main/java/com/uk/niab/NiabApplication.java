37c254649155fa4170c704e93bd4c993e8090c6b
DivyaRamesh12 <divyaramesh1207@gmail.com>
2022-02-08 13:02:41 +0000

    init commit

---
+++ src/main/java/com/uk/niab/NiabApplication.java
@@ -0,0 +1,13 @@
+package com.uk.niab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uk/niab/NiabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uk.niab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
